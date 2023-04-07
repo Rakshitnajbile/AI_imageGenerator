7ba5bb4b9e584e9c0ae38e11aa3d2b58f4d3994e
Rakshitnajbile <42490094+Rakshitnajbile@users.noreply.github.com>
2023-04-07 14:50:24 +0530

    logo modified

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,9 +1,12 @@
 import download from './download.png';
 import logo from './logo.svg';
 import preview from './preview.png';
-
+import logoblack from './logoblack.svg';
+import logobbg from './logobbg.png'
 export {
   download,
   logo,
   preview,
+  logoblack,
+  logobbg,
 };
